a34b76f8e4170c215cc89c68268c2e1f6a19bea0
JensBrandels <brandelsjens@gmail.com>
2024-05-30 11:54:11 +0200

    index.js setup done and added a base file structure to the project

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 app.use(express.json());
+//here goes routes
+
+const mongoURI = process.env.DB_URI;
+
+mongoose.connect(mongoURI);
+
+mongoose.connection.once("open", () => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is up and running on PORT: ${process.env.PORT}`);
+  });
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(`Failed to connect to MongoDB: ${err.message}`);
+});
