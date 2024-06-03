d45d8f1bae1ff256db2d595f7114d9d171d970ee
JensBrandels <brandelsjens@gmail.com>
2024-06-03 14:21:43 +0200

    movies put and delete done

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -60,6 +60,13 @@ movieRoute.get(
   async (req, res) => {
     try {
       const movieId = req.params.id;
+
+      const findMovieId = await Movie.findById(movieId);
+
+      if (!findMovieId) {
+        return res.status(400).send("Id is wrong");
+      }
+
       const foundMovie = await Movie.findById(movieId);
       if (!foundMovie) {
         return res.status(404).json({ error: "Movie not found" });
@@ -67,6 +74,91 @@ movieRoute.get(
       res.status(200).send(foundMovie);
     } catch (error) {
       res.status(500).send(error.message);
+    }
+  }
+);
+
+movieRoute.delete(
+  "/movies/:id",
+  authenticateToken,
+  checkRole(["admin"]),
+  async (req, res) => {
+    try {
+      const movieId = req.params.id;
+
+      const findMovieId = await Movie.findById(movieId);
+
+      if (!findMovieId) {
+        return res.status(400).send("Id is wrong");
+      }
+
+      const deleteMovie = await Movie.findByIdAndDelete(movieId);
+      if (!deleteMovie) {
+        return res.status(404).json({
+          error: "Movie not found and could therefore not be deleted!",
+        });
+      }
+      res
+        .status(200)
+        .send({ msg: "deleted movie successfully", deleted: deleteMovie });
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  }
+);
+
+movieRoute.put(
+  "/movies/:id",
+  authenticateToken,
+  checkRole(["admin"]),
+  async (req, res) => {
+    try {
+      const movieId = req.params.id;
+
+      const findMovieId = await Movie.findById(movieId);
+
+      if (!findMovieId) {
+        return res.status(400).send("Id is wrong");
+      }
+
+      const { title, director, releaseYear, genre } = req.body;
+
+      // Check if another movie with the same attributes exists
+      const existingMovie = await Movie.findOne({
+        _id: { $ne: movieId }, // Exclude the current movie
+        title,
+        director,
+        releaseYear,
+        genre,
+      });
+
+      if (existingMovie) {
+        return res.status(400).json({
+          error:
+            "A movie with the same title, director, release year, and genre already exists.",
+        });
+      }
+
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        movieId,
+        { title, director, releaseYear, genre },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+
+      if (!updatedMovie) {
+        return res.status(404).json({
+          error: "Movie not found and could therefore not be updated!",
+        });
+      }
+
+      res
+        .status(200)
+        .send({ msg: "Movie updated successfully!", update: updatedMovie });
+    } catch (error) {
+      res.status(400).send(error.message);
     }
   }
 );
